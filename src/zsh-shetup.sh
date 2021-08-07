809edb5b882a43e35f9bb331f3bfd3d7b7e85e05
Szymon Kmieć <74502684+saimon833@users.noreply.github.com>
2021-08-07 12:55:54 +0200

    Update zsh-shetup.sh

--- src/zsh-shetup.sh
+++ src/zsh-shetup.sh
@@ -3,9 +3,9 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 rm ~/.zshrc
 rm ~/.vimrc
-ln -s $PWD/.vimrc ~/.vimrc
-ln -s $PWD/.zshrc ~/.zshrc
-ln -s $PWD/.aliasrc ~/.aliasrc
+ln -s $PWD/vimrc ~/.vimrc
+ln -s $PWD/zshrc ~/.zshrc
+ln -s $PWD/aliasrc ~/.aliasrc
 rm ~/.oh-my-zsh/themes/jispwoso.zsh-theme
 ln -s $PWD/jispwoso.zsh-theme ~/.oh-my-zsh/themes/jispwoso.zsh-theme
 source .zshrc
