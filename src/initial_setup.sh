696c106d27ad0b4c828aa13874761057e382681d
Szymon Kmieć <74502684+saimon833@users.noreply.github.com>
2021-08-09 00:49:29 +0200

    some update

--- src/initial_setup.sh
+++ src/initial_setup.sh
@@ -1,8 +1,36 @@
 #!/usr/bin/bash
+ln -sf /usr/share/zoneinfo/Europe/Warsaw /etc/localtime
+hwclock --systohc
+sed -i '177s/.//' /etc/locale.gen
+sed -i '390s/.//' /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+echo "ArchBox" >> /etc/hostname
+echo "127.0.0.1 localhost" >> /etc/hosts
+echo "::1       localhost" >> /etc/hosts
+echo "127.0.1.1 ArchBox.localdomain Archbox" >> /etc/hosts
+echo root:password | chpasswd
+
+pacman -S grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers avahi xdg-user-dirs \
+xdg-utils gvfs cups hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack openssh rsync acpi acpi_call tlp virt-manager \
+qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft sof-firmware os-prober ntfs-3g
+
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+
+systemctl enable cups.service
+systemctl enable sshd
+systemctl enable reflector.timer
+systemctl enable fstrim.timer
+
+useradd -m szymon 
+echo szymon:password | chpasswd
+echo "szymon ALL=(ALL) ALL" >> /etc/sudoers.d/szymon
+
 systemctl start NetworkManager
 systemctl enable NetworkManager
 pacman -Syu
-pacman -S nvidia nvidia-utils xorg xorg-server gdm reflector
+pacman -S nvidia nvidia-utils xorg xorg-server gdm 
 systemctl enable gdm
 echo "Enabling multilib\n"
 sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
@@ -16,7 +44,6 @@ bashtop \
 gnome-terminal \
 gnome-tweak-tool \
 gnome-control-center \
-xdg-user-dirs-gtk \
 eog \
 evince \
 vlc \
@@ -52,9 +79,7 @@ sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcryp
 ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 \
 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader \
 lib32-libxml2 lib32-sdl2 lib32-freetype2 lib32-dbus
-steam \
-qemu qemu-arch-extra ovmf bridge-utils dnsmasq vde2 \
-ebtables iptables virt-manager virt-viewer
+steam  virt-viewer
 echo "Configuring nvida\n"
 nvidia-xconfig
 echo "Enabling & configuring libvirt\n"
--- src/zsh-shetup.sh
+++ src/zsh-shetup.sh
@@ -3,10 +3,10 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 rm ~/.zshrc
 rm ~/.vimrc
-ln -s $PWD/vimrc ~/.vimrc
-ln -s $PWD/zshrc ~/.zshrc
-ln -s $PWD/aliasrc ~/.aliasrc
+cp vimrc ~/.vimrc
+cp zshrc ~/.zshrc
+cp aliasrc ~/.aliasrc
 rm ~/.oh-my-zsh/themes/jispwoso.zsh-theme
-ln -s $PWD/jispwoso.zsh-theme ~/.oh-my-zsh/themes/jispwoso.zsh-theme
+cp ispwoso.zsh-theme ~/.oh-my-zsh/themes/jispwoso.zsh-theme
 source .zshrc
 
