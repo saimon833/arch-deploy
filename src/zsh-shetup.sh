931583269078d41584e47be217f649c7ec28310e
Szymon Kmieć <74502684+saimon833@users.noreply.github.com>
2021-08-25 21:59:13 +0000

    Update zsh-shetup.sh

--- src/zsh-shetup.sh
+++ src/zsh-shetup.sh
@@ -7,6 +7,6 @@ cp vimrc ~/.vimrc
 cp zshrc ~/.zshrc
 cp aliasrc ~/.aliasrc
 rm ~/.oh-my-zsh/themes/jispwoso.zsh-theme
-cp ispwoso.zsh-theme ~/.oh-my-zsh/themes/jispwoso.zsh-theme
+cp jispwoso.zsh-theme ~/.oh-my-zsh/themes/jispwoso.zsh-theme
 source ~/.zshrc
 
