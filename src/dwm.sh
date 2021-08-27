566e5181415938423428538fe29aca29db09e1a5
saimon833 <74502684+saimon833@users.noreply.github.com>
2021-08-27 22:03:06 +0200

    Changed terminal to st

--- src/dwm.sh
+++ src/dwm.sh
@@ -7,7 +7,7 @@ cd $HOME/git
 git clone https://github.com/saimon833/dwm
 git clone https://github.com/saimon833/dmenu
 git clone https://github.com/saimon833/slstatus
-
+git clone https://github.com/saimon833/st
 cd $HOME/git/dwm
 sudo make clean install
 
@@ -17,9 +17,10 @@ sudo make clean install
 cd $HOME/git/slstatus
 sudo make clean install 
 
+cd $HOME/git/st
+sudo make clean install
+
 cp $HOME/git/arch-deploy/src/xinitrc $HOME/.xinitrc
-mkdir $HOME/.config/alacritty/
-cp $HOME/git/arch-deploy/src/alacritty.yml $HOME/.config/alacritty/alacritty.yml
 mkdir $HOME/.config/gtk-3.0/
 cp $HOME/git/arch-deploy/src/settings.ini $HOME/.config/gtk-3.0/settings.ini
 cp $HOME/git/arch-deploy/src/gtkrc-2.0 $HOME/.gtkrc-2.0
