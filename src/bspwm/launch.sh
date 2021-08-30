7f44259b297d95d2d2aea15d03bb927ff7924e7f
saimon833 <74502684+saimon833@users.noreply.github.com>
2021-08-30 12:14:52 +0200

    Added bspwm

---
+++ src/bspwm/launch.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall -q polybar
+# If all your bars have ipc enabled, you can also use 
+# polybar-msg cmd quit
+#if type "xrandr"; then
+#	  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+#		      MONITOR=$m polybar --reload example &
+#		        done
+#		else
+#			  polybar --reload example &
+#fi
+# Launch bar1 and bar2
+echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
+
+echo "Bars launched..."
---
+++ src/bspwm/setup.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/zsh
+sudo pacman -S nvidia nvidia-utils xorg nvidia-settings xorg xorg-xinit flameshot nemo pavucontrol eog evince bspwm sxhkd alacritty nitrogen xautolock dmenu
+
+paru -S polybar pacman-contrib ttf-font-awesome siji-git
+cd $HOME/git
+git clone https://github.com/saimon833/slock
+cd $HOME/git/slock
+sudo make clean install
+cp $HOME/git/arch-seploy/src/bspwm/xinitrc .xinitrc
+mkdir $HOME/.config/alacritty/
+ln -s $HOME/git/arch-seploy/src/bspwm/alacritty.yml $HOME/.config/alacritty/alacritty.yml
+install -Dm755 $HOME/git/arch-seploy/src/bspwm/bspwmrc $HOME/.config/bspwm/bspwmrc
+install -Dm644 $HOME/git/arch-seploy/src/bspwm/sxhkdrc $HOME/.config/sxhkd/sxhkdrc
+mkdir $HOME/.config/polybar
+ln -s $HOME/git/arch-seploy/src/bspwm/config $HOME/.config/polybar/config
+ln - $HOME/git/arch-seploy/src/bspwm/launch.sh $HOME/.config/polybar/launch.sh
+chmod +x $HOME/.config/polybar/launch.sh
+mkdir $HOME/.config/gtk-3.0/
+cp $HOME/git/arch-deploy/src/settings.ini $HOME/.config/gtk-3.0/settings.ini
+cp $HOME/git/arch-deploy/src/gtkrc-2.0 $HOME/.gtkrc-2.0
+
