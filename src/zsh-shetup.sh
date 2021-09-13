837b3bb92627e259e627c8108659bc2a17be7cd2
saimon833 <74502684+saimon833@users.noreply.github.com>
2021-09-13 16:31:06 +0200

    Updated zsh

--- src/zsh-shetup.sh
+++ src/zsh-shetup.sh
@@ -2,7 +2,12 @@
 cd $HOME/git/arch-deploy/src/
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10
+[ -d "$HOME/.local/share/fonts" ] || mkdir $HOME/.local/share/fonts
+wget -O $HOME/.local/share/fonts https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
+wget -O $HOME/.local/share/fonts https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
+wget -O $HOME/.local/share/fonts https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
+wget -O $HOME/.local/share/fonts https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
 rm ~/.zshrc
 rm ~/.vimrc
 ln -s $HOME/git/arch-deploy/src/vimrc ~/.vimrc 
