4c8bd64184bb521c6434d0dd03ca0041fc5550e8
saimon833 <74502684+saimon833@users.noreply.github.com>
2021-09-19 13:04:43 +0200

    Changed install scripts

--- bspwm/setup.sh
+++ bspwm/setup.sh
@@ -1,18 +1,11 @@
 #!/usr/bin/zsh
-sudo pacman -S nvidia-dkms nvidia-utils xorg nvidia-settings xorg xorg-xinit flameshot nemo pavucontrol eog evince bspwm sxhkd alacritty nitrogen ranger ueberzug
+sudo pacman -S nvidia-dkms nvidia-utils xorg nvidia-settings xorg xorg-xinit flameshot nemo pavucontrol eog evince bspwm sxhkd alacritty nitrogen ranger ueberzug dmenu
 paru -S polybar pacman-contrib ttf-font-awesome siji-git betterlockscreen j4-dmenu-desktop
-ln -s $HOME/git/arch-deploy/src/bspwm/xinitrc $HOME/.xinitrc
-mkdir $HOME/.config/alacritty/
-ln -s $HOME/git/arch-deploy/src/bspwm/alacritty.yml $HOME/.config/alacritty/alacritty.yml
-mkdir $HOME/.config/bspwm
-ln -s $HOME/git/arch-deploy/src/bspwm/bspwmrc $HOME/.config/bspwm/bspwmrc
-mkdir $HOME/.config/sxhkd
-ln -s $HOME/git/arch-deploy/src/bspwm/sxhkdrc $HOME/.config/sxhkd/sxhkdrc
-mkdir $HOME/.config/polybar
-ln -s $HOME/git/arch-deploy/src/bspwm/config $HOME/.config/polybar/config
-ln -s $HOME/git/arch-deploy/src/bspwm/launch.sh $HOME/.config/polybar/launch.sh
+stow -t $HOME/ -d $HOME/git/arch-deploy/dotfiles xinit
+stow -t $HOME/ -d $HOME/git/arch-deploy/dotfiles alacritty
+stow -t $HOME/ -d $HOME/git/arch-deploy/dotfiles bspwm
+stow -t $HOME/ -d $HOME/git/arch-deploy/dotfiles sxhkd 
+stow -t $HOME/ -d $HOME/git/arch-deploy/dotfiles polybar
 chmod +x $HOME/.config/polybar/launch.sh
-mkdir $HOME/.config/gtk-3.0/
-cp $HOME/git/arch-deploy/src/settings.ini $HOME/.config/gtk-3.0/settings.ini
-cp $HOME/git/arch-deploy/src/gtkrc-2.0 $HOME/.gtkrc-2.0
-
+stow -t $HOME/ -d $HOME/git/arch-deploy/dotfiles gtk2.0
+stow -t $HOME/ -d $HOME/git/arch-deploy/dotfiles gtk-3.0
