60b8ee97b81ec9edc26be1f97807e5eef62a38ec
saimon833 <74502684+saimon833@users.noreply.github.com>
2021-09-23 16:28:21 +0200

    Removed nemo stow

--- install/zsh-shetup.sh
+++ install/zsh-shetup.sh
@@ -15,6 +15,5 @@ stow -t $HOME/ -d $HOME/git/arch-deploy/dotfiles/ neofetch
 stow -t $HOME/ -d $HOME/git/arch-deploy/dotfiles/ htop
 stow -t $HOME/ -d $HOME/git/arch-deploy/dotfiles/ xdg-dirs
 stow -t $HOME/ -d $HOME/git/arch-deploy/dotfiles/ scripts
-stow -t $HOME/ -d $HOME/git/arch-deploy/dotfiles/ nemo
 stow -t $HOME/ -d $HOME/git/arch-deploy/dotfiles/ makepkg
 source ~/.zshrc
